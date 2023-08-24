9d835b3b5ea772801c686b43e4b07b24bdb4c8fe
Santiago Ramirez <94815926+SantiiRepair@users.noreply.github.com>
2023-08-24 03:38:51 +0000

    Update 4 files

--- vault_decryptor/__main__.py
+++ vault_decryptor/__main__.py
@@ -7,30 +7,15 @@ import requests
 from pathlib import Path
 from decryptor.helpers.bash import bash
 
+# used to hide asyncio annoying warning
 shutup.please()
 c = os.path.dirname(os.path.abspath(__file__))
 
 
 async def _main():
     js_packages = ["@metamask/browser-passworder"]
-    jsmod_dir = Path(f"{c}/jsmod")
-    pymod_dir = Path(f"{c}/pymod")
-    passworder_raw = "https://raw.githubusercontent.com/MetaMask/vault-decryptor/master/app/lib.js"
-    if not os.path.exists(jsmod_dir):
-        os.makedirs(jsmod_dir)
-    if not os.path.exists(pymod_dir):
-        os.makedirs(pymod_dir)
-    if not os.path.exists(f"{jsmod_dir}/passworder.js"):
-        passworderjs = requests.get(url=passworder_raw)
-        with open(f"{jsmod_dir}/passworder.js", "w") as f:
-            f.write(passworderjs.content.decode())
     for package in js_packages:
         await bash(f"npm i -g {package}")
-    for jsmod in jsmod_dir.glob("*.js"):
-        js2py.translate_file(
-            str(jsmod),
-            str(jsmod).replace("jsmod", "pymod").replace(".js", ".py"),
-        )
 
 
 if __name__ == "__main__":
