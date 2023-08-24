7bad6844329fe15a0dce3fdea092447f21acefcc
Santiago Ramirez <94815926+SantiiRepair@users.noreply.github.com>
2023-08-24 02:23:42 +0000

    Update 8 files

--- vault_decryptor/__main__.py
+++ vault_decryptor/__main__.py
@@ -2,6 +2,7 @@ import os
 import js2py
 import shutup
 import asyncio
+import argparse
 import requests
 from pathlib import Path
 from decryptor.helpers.bash import bash
@@ -14,20 +15,20 @@ async def _main():
     js_packages = ["@metamask/browser-passworder"]
     jsmod_dir = Path(f"{c}/jsmod")
     pymod_dir = Path(f"{c}/pymod")
-    bundle_raw = "https://raw.githubusercontent.com/MetaMask/vault-decryptor/master/bundle.js"
+    passworder_raw = "https://raw.githubusercontent.com/MetaMask/vault-decryptor/master/app/lib.js"
     if not os.path.exists(jsmod_dir):
         os.makedirs(jsmod_dir)
     if not os.path.exists(pymod_dir):
         os.makedirs(pymod_dir)
-    if not os.path.exists(f"{jsmod_dir}/bundle.js"):
-        bundlejs = requests.get(url=bundle_raw)
-        with open(f"{jsmod_dir}/bundle.js", "w") as f:
-            f.write(bundlejs.content.decode())
+    if not os.path.exists(f"{jsmod_dir}/passworder.js"):
+        passworderjs = requests.get(url=passworder_raw)
+        with open(f"{jsmod_dir}/passworder.js", "w") as f:
+            f.write(passworderjs.content.decode())
     for package in js_packages:
         await bash(f"npm i -g {package}")
     for jsmod in jsmod_dir.glob("*.js"):
         js2py.translate_file(
-            jsmod,
+            str(jsmod),
             str(jsmod).replace("jsmod", "pymod").replace(".js", ".py"),
         )
 
