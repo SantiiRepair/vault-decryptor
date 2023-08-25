bcd0f705ff6aeb9de9692c01b0110c679cdc36b1
Santiago Ramirez <94815926+SantiiRepair@users.noreply.github.com>
2023-08-25 01:04:34 +0000

    Update 5 files

--- vault_decryptor/__main__.py
+++ vault_decryptor/__main__.py
@@ -1,11 +1,8 @@
 import os
-import js2py
 import shutup
 import asyncio
 import argparse
-import requests
-from pathlib import Path
-from decryptor.helpers.bash import bash
+from vault_decryptor.helpers.bash import bash
 
 # used to hide asyncio annoying warning
 shutup.please()
@@ -13,8 +10,8 @@ c = os.path.dirname(os.path.abspath(__file__))
 
 
 async def _main():
-    js_packages = ["@metamask/browser-passworder"]
-    for package in js_packages:
+    nu_packages = ["@metamask/browser-passworder"]
+    for package in nu_packages:
         await bash(f"npm i -g {package}")
 
 
