2e0010a3b1ed6c9e64716435c7734f4e0df5f29f
Santiago Ramirez <94815926+SantiiRepair@users.noreply.github.com>
2023-08-26 01:51:06 +0000

    Update decrypt.py

--- vault_decryptor/modules/decrypt.py
+++ vault_decryptor/modules/decrypt.py
@@ -8,19 +8,19 @@ from vault_decryptor.modules.key_from_password import key_from_password
 def decrypt(password: str, text: str, key: str = None) -> Any:
     try:
         payload = json.loads(text)
-        if "KeyringController" in text:
-            salt = payload["KeyringController"]["vault"]["salt"]
-            crypto_key = key or key_from_password(password, salt)
-            result = decrypt_with_key(crypto_key, payload)
-            return result
-        if "vault" in text:
-            salt = payload["vault"]["salt"]
-            crypto_key = key or key_from_password(password, salt)
-            result = decrypt_with_key(crypto_key, payload)
-            return result
-        salt = payload["salt"]
+    except ValueError:
+        print(colored("[ERROR]: The JSON file is invalid", "red"))
+    if "KeyringController" in text:
+        salt = payload["KeyringController"]["vault"]["salt"]
         crypto_key = key or key_from_password(password, salt)
         result = decrypt_with_key(crypto_key, payload)
         return result
-    except ValueError:
-        print(colored("[ERROR]: The JSON file is invalid", "red"))
+    if "vault" in text:
+        salt = payload["vault"]["salt"]
+        crypto_key = key or key_from_password(password, salt)
+        result = decrypt_with_key(crypto_key, payload)
+        return result
+    salt = payload["salt"]
+    crypto_key = key or key_from_password(password, salt)
+    result = decrypt_with_key(crypto_key, payload)
+    return result
