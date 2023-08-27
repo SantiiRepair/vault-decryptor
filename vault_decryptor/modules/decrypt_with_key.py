ed0e5e4cc3040c8176e500ffa2db7619ea607833
Santiago Ramirez <94815926+SantiiRepair@users.noreply.github.com>
2023-08-27 04:40:38 +0000

    Update decrypt_with_key.py

--- vault_decryptor/modules/decrypt_with_key.py
+++ vault_decryptor/modules/decrypt_with_key.py
@@ -7,12 +7,13 @@ from Crypto.Cipher import AES
 
 def decrypt_with_key(key: bytes, payload: Dict) -> Dict:
     try:
-        data = base64.b64decode(payload["data"])[:-16]
+        data = base64.b64decode(payload["data"])
         iv = base64.b64decode(payload["iv"])
         cipher = AES.new(key, mode=AES.MODE_GCM, nonce=iv)
-        decrypted_data = cipher.decrypt(data)
-        print(decrypted_data)
-        decrypted = json.loads(decrypted_data)
+        decrypted_bytes = cipher.decrypt(data)
+        decrypted_text = decrypted_bytes.strip().decode("utf-8")
+        print(decrypted_text)
+        decrypted = json.loads(decrypted_text)
         return decrypted
     except (Exception, ValueError) as e:
         # if isinstance(e, UnicodeDecodeError):
