410d3da989e8b33edbca68ff379b3454a2c83a8d
Santiago Ramirez <94815926+SantiiRepair@users.noreply.github.com>
2023-08-27 07:06:18 +0000

    Update 6 files

--- golang/main.go
+++ golang/main.go
@@ -1,54 +1,39 @@
 package main
 
 import (
-    "crypto/aes"
-    "crypto/cipher"
-    "crypto/sha256"
-    "encoding/base64"
-    "encoding/json"
-    "fmt"
-    "golang.org/x/crypto/pbkdf2"
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/sha256"
+	"encoding/base64"
+	"encoding/json"
+	"fmt"
+	"golang.org/x/crypto/pbkdf2"
 )
-
-type Payload struct {
-    Data string `json:"data"`
-    Salt string `json:"salt"`
-    Iv   string `json:"iv"`
-}
-
-type Vault struct {
-    Type string `json:"type"`
-    Data struct {
-        Mnemonic         []byte `json:"mnemonic"`
-        NumberOfAccounts int    `json:"numberOfAccounts"`
-        HDPath           string `json:"hdPath"`
-    } `json:"data"`
-}
 
 var wallet = []byte(``)
 
 func main() {
-    var payload Payload
-    json.Unmarshal(wallet, &payload)
+	var payload Payload
+	json.Unmarshal(wallet, &payload)
 
-    iv, _ := base64.StdEncoding.DecodeString(payload.Iv)
-    salt, _ := base64.StdEncoding.DecodeString(payload.Salt)
-    data, _ := base64.StdEncoding.DecodeString(payload.Data)
+	iv, _ := base64.StdEncoding.DecodeString(payload.Iv)
+	salt, _ := base64.StdEncoding.DecodeString(payload.Salt)
+	data, _ := base64.StdEncoding.DecodeString(payload.Data)
 
-    password := "Elmetamask1"
+	password := "Elmetamask1"
 
-    key := pbkdf2.Key([]byte(password), salt, 10000, 32, sha256.New)
-    block, _ := aes.NewCipher(key)
+	key := pbkdf2.Key([]byte(password), salt, 10000, 32, sha256.New)
+	block, _ := aes.NewCipher(key)
 
-    // Initialization Vector size is fixed to 16 bytes.
-    gcm, _ := cipher.NewGCMWithNonceSize(block, len(iv))
-    plaintext, err := gcm.Open(nil, iv, data, nil)
+	// Initialization Vector size is fixed to 16 bytes.
+	gcm, _ := cipher.NewGCMWithNonceSize(block, len(iv))
+	plaintext, err := gcm.Open(nil, iv, data, nil)
 
-    if err != nil {
-        panic(err)
-    }
+	if err != nil {
+		panic(err)
+	}
 
-    var vault []Vault
-    json.Unmarshal(plaintext, &vault)
-    fmt.Println(string(vault[0].Data.Mnemonic))
+	var vault []Vault
+	json.Unmarshal(plaintext, &vault)
+	fmt.Println(string(vault[0].Data.Mnemonic))
 }
---
+++ golang/modules/key_from_password.go
@@ -0,0 +1,15 @@
+package modules
+
+import (
+    "crypto/aes"
+    "crypto/cipher"
+    "crypto/sha256"
+    "encoding/base64"
+    "encoding/json"
+    "fmt"
+    "golang.org/x/crypto/pbkdf2"
+)
+
+func key_from_password(password: str) {
+	return pbkdf2.Key([]byte(password), salt, 10000, 32, sha256.New)
+}
