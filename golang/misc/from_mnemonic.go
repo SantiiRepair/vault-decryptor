f3fffb7605e0ea215741b88f47e639a11d9bc9f5
Santiago Ramirez <94815926+SantiiRepair@users.noreply.github.com>
2023-09-04 17:21:11 +0000

    Update 6 files

--- golang/misc/from_mnemonic.go
+++ golang/misc/from_mnemonic.go
@@ -1,37 +1,33 @@
 package misc
 
 import (
-	"crypto/ecdsa"
 	"os"
 
-	"github.com/btcsuite/btcd/btcutil/hdkeychain"
-	"github.com/btcsuite/btcd/chaincfg"
-	"github.com/ethereum/go-ethereum/common/hexutil"
-	"github.com/ethereum/go-ethereum/crypto"
 	color "github.com/fatih/color"
-	bip39 "github.com/tyler-smith/go-bip39"
+	"github.com/miguelmota/go-ethereum-hdwallet"
 )
 
 func FromMnemonic(mnemonic string, password string) ([]string, error) {
 	red := color.New(color.FgRed)
-	seed := bip39.NewSeed(mnemonic, password)
-	masterKey, err := hdkeychain.NewMaster(seed, &chaincfg.MainNetParams)
+	wallet, err := hdwallet.NewFromMnemonic(mnemonic)
 	if err != nil {
 		red.Sprintf("[ERROR]: %s", err)
 		os.Exit(1)
 	}
 
-	privateKey, err := masterKey.ECPrivKey()
-	privateKeyECDSA := privateKey.ToECDSA()
-	publicKey := privateKeyECDSA.Public()
-
-	publicKeyECDSA, ok := publicKey.(*ecdsa.PublicKey)
-	if !ok {
-		red.Println("[ERROR]: Could not convert the public key type.")
+	path := hdwallet.MustParseDerivationPath("m/44'/60'/0'/0/0")
+	account, err := wallet.Derive(path, false)
+	if err != nil {
+		red.Sprintf("[ERROR]: %s", err)
 		os.Exit(1)
 	}
 
-	a, p := crypto.PubkeyToAddress(*publicKeyECDSA).String(), hexutil.Encode(crypto.FromECDSA(privateKeyECDSA))[2:]
+	a := account.Address.Hex()
+	p, err := wallet.PrivateKeyHex(account)
+	if err != nil {
+		red.Sprintf("[ERROR]: %s", err)
+		os.Exit(1)
+	}
 
 	return []string{a, p}, nil
 }
